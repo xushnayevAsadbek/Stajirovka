233bf3cf31e1a1de66f2364c0c02613e94ca24d5
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-07-17 01:21:05 +0500

    intial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabContents = document.querySelectorAll('.tab_content'),
+		tabParents = document.querySelector('.tabheader__items')
+
+	function hideTabContents() {
+		tabContents.forEach(tabContent => {
+			tabContent.classList.add('hide')
+			tabContent.classList.remove('show')
+		})
+
+		tabs.forEach(tab => {
+			tab.classList.remove('tabheader__item_active')
+		})
+	}
+	function showTabContents(index = 0) {
+		tabContents[index].classList.add('show', 'fade')
+		tabContents[index].classList.remove('hide')
+		tabs[index].classList.add('tabheader__item_active')
+	}
+
+	hideTabContents()
+	showTabContents()
+
+	tabParents.addEventListener('click', event => {
+		const target = event.target
+
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((tab, index) => {
+				if (target === tab) {
+					hideTabContents()
+					showTabContents(index)
+				}
+			})
+		}
+	})
+	// Loader
+	const loaderWrapper = document.querySelector('.loader-wrapper')
+	setTimeout(() => {
+		loaderWrapper.style.display = 'none'
+	}, 1500)
+})
