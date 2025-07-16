f70c944e23e47cde6f24f53c2507896edb23457d
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-07-17 01:31:55 +0500

    main section add img and loader

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	const loaderWrapper = document.querySelector('.loader-wrapper')
 	setTimeout(() => {
 		loaderWrapper.style.display = 'none'
-	}, 1500)
+	}, 1000)
 })
