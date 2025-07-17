1367585297ca03cbd097922817762524af86fc67
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-07-17 11:56:12 +0500

    modal add commit

--- js/script.js
+++ js/script.js
@@ -43,4 +43,43 @@ window.addEventListener('DOMContentLoaded', () => {
 	}, 1000)
 
 	// Modal
+
+	const modalOpenBtns = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-modal-close]'),
+		modalContent = document.querySelector('.modal__content')
+
+	function openModal() {
+		modalContent.classList.add('modal_fade')
+		modal.classList.add('show')
+		modal.classList.remove('hide')
+		document.body.style.overflow = 'hidden'
+		clearInterval(modalTimerId)
+	}
+
+	function closeModal() {
+		modal.classList.add('hide')
+		modal.classList.remove('show')
+		document.body.style.overflow = ''
+	}
+
+	modalOpenBtns.forEach(btn => {
+		btn.addEventListener('click', openModal)
+	})
+
+	modalCloseBtn.addEventListener('click', closeModal)
+
+	modal.addEventListener('click', event => {
+		if (event.target === modal) {
+			closeModal()
+		}
+	})
+
+	document.addEventListener('keydown', event => {
+		if (event.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal()
+		}
+	})
+
+	const modalTimerId = setTimeout(openModal, 5000)
 })
